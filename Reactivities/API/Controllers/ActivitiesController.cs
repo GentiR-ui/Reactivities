15c8c907b40a0960437799c3f54667503045e209
GentiR-ui <genti.rj@gmail.com>
2025-10-02 23:38:22 +0200

    End of section 2

---
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,19 @@
+using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+
+
+
+namespace API.Controllers;
+
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+}
